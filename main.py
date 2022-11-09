12d9a4d3d446f0e8550a81307c3c0142fea9800b
henz <hans@bygglet>
2022-11-09 11:25:21 +0100

    add bad code to trigger codescanning

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ def main():
     if False:
         print("hej")
 
+    (lambda f, n: f(f, n))(lambda f, n: [(not n % 3 and "fizz" or "") + (not n % 5 and "buzz" or "") or n] + f(f, n+1) if n <= 100 else [], 1)
+
 if __name__ == "__main__":
     main()
